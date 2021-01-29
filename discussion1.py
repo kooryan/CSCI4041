17da53b346f003130dff111f7475e44aedba7523
Ryan K <kooryan03@gmail.com>
2021-01-29 11:26:00 -0600

    first commit

---
+++ discussion1.py
@@ -0,0 +1,16 @@
+A = [5, 2, 8, 4, 6, 1, 3, 7]
+
+count = 0
+
+for j in range(1, len(A)):
+    i = j - 1
+    key = A[j]
+
+    while (i >= 0) and (A[i] > key):
+        A[i + 1] = A[i]
+        i = i - 1
+        count = count + 1
+
+    A[i + 1] = key
+
+print("The array is: ", A)
