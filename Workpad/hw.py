058a9fa985b12ad114eb38def3e2caeadae487a0
Ryan K <kooryan03@gmail.com>
2021-02-10 14:30:44 -0600

    Add file for pseudo code and testing our random stuff

---
+++ Workpad/hw.py
@@ -0,0 +1,19 @@
+A = [7, 6, 5, 4, 3, 2, 1]
+
+i = 1
+count = 1
+while i < len(A):
+    x = A[i]
+    j = i - 1
+    count += 3
+    while j >= 0 and A[j] > x:
+        A[j + 1] = A[j]
+        j -= 1
+        count += 3
+    A[j+1] = x
+    i += 1
+    count += 3
+count += 1
+
+print(count)
+print(A)
